3db1b17c20d88bdff8a9178c8ba94feae5df78f2
Elvin Latifi <88437770+elvinlatifi@users.noreply.github.com>
2021-10-14 20:13:55 +0200

    Added basic Player class (not fully implemented), removed Spell interface

---
+++ src/main/java/PlayerInterface.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public interface PlayerInterface {
+    int getHPValue();
+
+    void decreaseHP(int amount);
+
+    void increaseHP(int amount);
+
+    int getManaValue();
+
+    void decreaseMana(int amount);
+
+    void increaseMana(int amount);
+
+    boolean inventoryFull();
+
+    ArrayList<Weapon> getInventory();
+
+    void consumePotion(Potion potion);
+
+    void equip(Weapon weapon);
+
+    int getLevel();
+
+    void rewardExp(int gainedExp);
+
+    Action getAction();
+}
--- src/main/java/Spell.java
+++
@@ -1,7 +0,0 @@
-public interface Spell {
-    Element getElement();
-
-    int getManaCost();
-
-    int getDamageValue();
-}
