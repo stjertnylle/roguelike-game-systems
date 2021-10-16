abb54b8ee5ef3cda067069833de716d7fc6af5a3
arvinrivers <90714951+arvinrivers@users.noreply.github.com>
2021-10-16 20:33:22 +0200

    added tests for when player gets to hit first.

--- src/main/java/Combat.java
+++ src/main/java/Combat.java
@@ -1,39 +1,42 @@
 public class Combat {
-    Entity player;
-    Entity monster, winner, loser;
+    Entity player, monster, winner, loser;
 
-    public Combat(Entity player, Entity monster){
-        this.player= player;
+    public Combat(Entity player,Entity monster){
+        this.player = player;
         this.monster = monster;
     }
 
-   public Entity startCombat(){
-       while(this.player.getCurrentHP() > 0 && this.monster.getCurrentHP() > 0){
-           Action playerAction = new LightAttack(10);  //hardcoded values needing to change
-           Action monsterAction = new HeavyAttack(10); //hardcoded values needing to change
-           Action fastestAction = getFastestAction(playerAction, monsterAction);
-           if ( fastestAction == playerAction ) {
-               playerAction.use(this.player, this.monster);}
-           else
-               monsterAction.use(this.monster, this.player);
-       }
-       if (this.player.getCurrentHP() <= 0){
-           gameOver();
-           loser = player;
-           return winner = monster;
-       }
-       else{
-           endCombat();
-           loser = monster;
-           return winner = player;
-       }
-   }
+    public Entity startCombat(){
+        while (this.player.getCurrentHP() > 0 && this.monster.getCurrentHP() > 0) {
+            Action playerAction = ((StandardPlayer)this.player).getAction();
+            Action monsterAction = ((Monster)this.monster).getAction(this.player.getCurrentHP());
+            Action fastestAction = getFastestAction(playerAction,monsterAction);
+            if ( fastestAction == playerAction ) {
+                playerAction.use(this.player,this.monster);
+                if ( monster.getCurrentHP() > 0 )
+                    monsterAction.use(this.monster,this.player);
+            } else {
+                monsterAction.use(this.monster,this.player);
+                if ( player.getCurrentHP() > 0 )
+                    playerAction.use(this.player,this.monster);
+            }
+        }
+        if ( this.player.getCurrentHP() <= 0 ) {
+            gameOver();
+            loser = player;
+            return winner = monster;
+        } else {
+            endCombat();
+            loser = monster;
+            return winner = player;
+        }
+    }
 
-   public Action getFastestAction(Action a1, Action a2){
-        if (a1.getSpeed() < a2.getSpeed())
+    public Action getFastestAction(Action a1,Action a2){
+        if ( a1.getSpeed() < a2.getSpeed() )
             return a2;
         else return a1;
-   }
+    }
 
     public void endCombat(){
         //TODO determine what happens when combat ends
--- src/test/java/CombatSystemTest.java
+++ src/test/java/CombatSystemTest.java
@@ -1,78 +1,14 @@
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CombatSystemTest {
-    Player player = new Player() {
+    StandardPlayer standardplayer = new StandardPlayer(10) {
         @Override
-        public int getHPValue(){
-            return 0;
-        }
-
-        @Override
-        public void decreaseHP(int amount){
-
-        }
-
-        @Override
-        public void increaseHP(int amount){
-
-        }
-
-        @Override
-        public int getManaValue(){
-            return 0;
-        }
-
-        @Override
-        public void decreaseMana(int amount){
-
-        }
-
-        @Override
-        public void increaseMana(int amount){
-
-        }
-
-        @Override
-        public boolean inventoryFull(){
-            return false;
-        }
-
-        @Override
-        public ArrayList<Weapon> getInventory(){
-            return null;
-        }
-
-        @Override
-        public void consumePotion(Potion potion){
-
-        }
-
-        @Override
-        public void equip(Weapon weapon){
-
-        }
-
-        @Override
-        public int getLevel(){
-            return 0;
-        }
-
-        @Override
-        public void rewardExp(int gainedExp){
-
-        }
-
-        @Override
-        public Action getAction(){
+        Action getAction(){
             return lightAttackFromPlayer;
         }
     };
-    StandardPlayer standardplayer = new StandardPlayer(10);
-    Vampire vampire = new Vampire(20);
+    Vampire vampire = new Vampire(50);
     LightAttack lightAttackFromPlayer = new LightAttack(10);
     LightAttack lightAttackFromMonster = new LightAttack(10);
     HeavyAttack heavyAttack = new HeavyAttack(10);
@@ -86,6 +22,28 @@ public class CombatSystemTest {
     @Test
     void getFastestActionWhenBothActionsSameSpeed(){
         assertEquals(lightAttackFromPlayer, combat.getFastestAction(lightAttackFromPlayer, lightAttackFromMonster));
+    }
+
+    @Test
+    void combatEndsWhenHPLessThanZero(){
+        vampire.setHP(1);
+        combat.startCombat();
+        assertTrue(vampire.getCurrentHP() < 0);
+    }
+
+    @Test
+    void startCombatReturnsCorrectWinner(){
+        vampire.setHP(1);
+        combat.startCombat();
+        assertEquals(standardplayer, combat.startCombat());
+    }
+
+    @Test
+    void monsterDoesNotGetToHitWhenItDies(){
+        standardplayer.setHP(1);
+        vampire.setHP(1);
+        combat.startCombat();
+        assertEquals(1, standardplayer.getCurrentHP());
     }
 
 
