542b7fc590467e8e69642edca42ddd28bbce9cf5
arvinrivers <90714951+arvinrivers@users.noreply.github.com>
2021-10-17 17:51:38 +0200

    added functionality for adding xp to increase level

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -4,18 +4,23 @@ public class Level {
     private int shownPlayerXP;
     private int xpToNextLevel;
     private final int MAX_LEVEL = 100;
+    private final double XP_INCREMENT = 1.5;
 
     public Level(int level){
         this.currentLevel = level;
         shownPlayerXP = 0;
         actualPlayerXP = 0;
         xpToNextLevel = 100;
+        for(int i = 1; i < currentLevel; i ++){
+            actualPlayerXP += xpToNextLevel;
+            xpToNextLevel *= XP_INCREMENT;
+        }
     }
 
     public void addXP(int XP){
         actualPlayerXP += XP;
         shownPlayerXP += XP;
-        if (shownPlayerXP >= xpToNextLevel && currentLevel < MAX_LEVEL){
+        while (shownPlayerXP >= xpToNextLevel && currentLevel < MAX_LEVEL){
             currentLevel ++;
             shownPlayerXP =  shownPlayerXP - xpToNextLevel;
             xpToNextLevel *= 1.5;
--- src/test/java/LevelTest.java
+++ src/test/java/LevelTest.java
@@ -22,4 +22,26 @@ public class LevelTest {
         levelOne.addXP(101);
         assertEquals(2, levelOne.getCurrentLevel());
     }
+
+    @Test
+    void levelIsIncreasedWhenAddingSufficientXPForLevelTwo(){
+        Level levelOne = new Level(2);
+        levelOne.addXP(200);
+        assertEquals(3, levelOne.getCurrentLevel());
+    }
+
+    @Test
+    void levelIsIncreasedCorrectlyWhenAddingSufficientXPForMoreThanOneLevelIncrease(){
+        Level levelOne = new Level(1);
+        levelOne.addXP(300);
+        assertEquals(3, levelOne.getCurrentLevel());
+    }
+
+    @Test
+    void levelIsNotIncreaseWhenAddingXPAtLevel100(){
+        Level levelHundred = new Level(100);
+        levelHundred.addXP(999999);
+        assertEquals(100, levelHundred.getCurrentLevel());
+    }
+
 }
