99b39928bf0ca96118d0621704961453f431f976
Elvin Latifi <88437770+elvinlatifi@users.noreply.github.com>
2021-10-19 21:42:55 +0200

    Added tests for exceptions

--- src/test/java/ElementTest.java
+++ src/test/java/ElementTest.java
@@ -3,112 +3,154 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ElementTest {
+
+    Element earth = new Earth();
+    Element fire = new Fire();
+    Element water = new Water();
+    Element air = new Air();
+
     @Test
     void testWaterAgainstFire() {
-        Element water = new Water();
-        Element fire = new Fire();
         assertEquals(1.5, water.getModifierAgainst(fire));
     }
 
     @Test
     void testWaterAgainstEarth() {
-        Element water = new Water();
-        Element earth = new Earth();
         assertEquals(0.5, water.getModifierAgainst(earth));
     }
 
     @Test
     void testWaterAgainstAir() {
-        Element water = new Water();
-        Element air = new Air();
         assertEquals(1, water.getModifierAgainst(air));
     }
 
     @Test
     void testWaterAgainstWater() {
-        Element water = new Water();
         assertEquals(1, water.getModifierAgainst(water));
     }
 
     @Test
+    void testExceptionThrownIfElementSentToWaterIsNull() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> water.getModifierAgainst(null));
+        assertEquals("Argument can not be null!", e.getMessage());
+    }
+
+    @Test
+    void testExceptionThrownIfElementSentToWaterDoesNotExistInSystem() {
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> water.getModifierAgainst(new Element() {
+            @Override
+            public double getModifierAgainst(Element element) {
+                return 0;
+            }
+        }));
+    }
+
+    @Test
     void testFireAgainstWater() {
-        Element fire = new Fire();
-        Element water = new Water();
         assertEquals(0.5, fire.getModifierAgainst(water));
     }
 
     @Test
     void testFireAgainstAir() {
-        Element fire = new Fire();
-        Element air = new Air();
         assertEquals(1.5, fire.getModifierAgainst(air));
     }
 
     @Test
     void testFireAgainstEarth() {
-        Element fire = new Fire();
-        Element earth = new Earth();
         assertEquals(1, fire.getModifierAgainst(earth));
     }
 
     @Test
     void testFireAgainstFire() {
-        Element fire = new Fire();
         assertEquals(1, fire.getModifierAgainst(fire));
     }
 
     @Test
+    void testExceptionThrownIfElementSentToFireIsNull() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> fire.getModifierAgainst(null));
+        assertEquals("Argument can not be null!", e.getMessage());
+    }
+
+    @Test
+    void testExceptionThrownIfElementSentToFireDoesNotExistInSystem() {
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> fire.getModifierAgainst(new Element() {
+            @Override
+            public double getModifierAgainst(Element element) {
+                return 0;
+            }
+        }));
+    }
+
+    @Test
     void testAirAgainstEarth() {
-        Element air = new Air();
-        Element earth = new Earth();
         assertEquals(1.5, air.getModifierAgainst(earth));
     }
 
     @Test
     void testAirAgainstFire() {
-        Element air = new Air();
-        Element fire = new Fire();
         assertEquals(0.5, air.getModifierAgainst(fire));
     }
 
     @Test
     void testAirAgainstWater() {
-        Element air = new Air();
-        Element water = new Water();
         assertEquals(1, air.getModifierAgainst(water));
     }
 
     @Test
     void testAirAgainstAir() {
-        Element air = new Air();
         assertEquals(1, air.getModifierAgainst(air));
     }
 
     @Test
+    void testExceptionThrownIfElementSentToAirIsNull() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> air.getModifierAgainst(null));
+        assertEquals("Argument can not be null!", e.getMessage());
+    }
+
+    @Test
+    void testExceptionThrownIfElementSentToAirDoesNotExistInSystem() {
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> air.getModifierAgainst(new Element() {
+            @Override
+            public double getModifierAgainst(Element element) {
+                return 0;
+            }
+        }));
+    }
+
+    @Test
     void testEarthAgainstWater() {
-        Element earth = new Earth();
-        Element water = new Water();
         assertEquals(1.5, earth.getModifierAgainst(water));
     }
 
     @Test
     void testEarthAgainstAir() {
-        Element earth = new Earth();
-        Element air = new Air();
         assertEquals(0.5, earth.getModifierAgainst(air));
     }
 
     @Test
     void testEarthAgainstFire() {
-        Element earth = new Earth();
-        Element fire = new Fire();
         assertEquals(1, earth.getModifierAgainst(fire));
     }
 
     @Test
     void testEarthAgainstEarth() {
-        Element earth = new Earth();
         assertEquals(1, earth.getModifierAgainst(earth));
+    }
+
+    @Test
+    void testExceptionThrownIfElementSentToEarthIsNull() {
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> earth.getModifierAgainst(null));
+        assertEquals("Argument can not be null!", e.getMessage());
+    }
+
+    @Test
+    void testExceptionThrownIfElementSentToEarthDoesNotExistInSystem() {
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> earth.getModifierAgainst(new Element() {
+            @Override
+            public double getModifierAgainst(Element element) {
+                return 0;
+            }
+        }));
     }
 
 }
