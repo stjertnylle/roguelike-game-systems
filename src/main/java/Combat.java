376311e76afaca26502a7c445ee2074e2fc5a970
arvinrivers <90714951+arvinrivers@users.noreply.github.com>
2021-10-25 14:25:08 +0200

    refactored startcombat into different methods, removed some unnecessary test cases, after feedback from inspection

--- src/main/java/Combat.java
+++ src/main/java/Combat.java
@@ -12,36 +12,44 @@ public class Combat {
             Action playerAction = ((Player) this.player).getAction();
             Action monsterAction = ((Monster) this.monster).getAction(this.player.getHealthRatio());
             Action fastestAction = getFastestAction(playerAction,monsterAction);
-            if ( fastestAction == playerAction ) {
-                playerAction.apply(this.monster);
-                if ( this.monster.getCurrentHP() > 0 )
-                    monsterAction.apply(this.player);
-            } else {
-                monsterAction.apply(this.player);
-                if ( this.player.getCurrentHP() > 0 )
-                    playerAction.apply(this.monster);
-            }
+            applyActions(playerAction,monsterAction,fastestAction);
         }
+        return endCombat();
+    }
+
+    private Entity endCombat(){
         if ( this.player.getCurrentHP() <= 0 ) {
             gameOver((Player)this.player);
             return this.monster;
         } else {
-            endCombat((Player) this.player,(Monster) this.monster);
+            rewardPlayerXP((Player) this.player,(Monster) this.monster);
             return this.player;
         }
     }
 
-    public Action getFastestAction(Action a1,Action a2){
+    private void applyActions(Action playerAction,Action monsterAction,Action fastestAction){
+        if ( fastestAction == playerAction ) {
+            playerAction.apply(this.monster);
+            if ( this.monster.getCurrentHP() > 0 )
+                monsterAction.apply(this.player);
+        } else {
+            monsterAction.apply(this.player);
+            if ( this.player.getCurrentHP() > 0 )
+                playerAction.apply(this.monster);
+        }
+    }
+
+    private Action getFastestAction(Action a1,Action a2){
         if ( a1.getSpeed() < a2.getSpeed() )
             return a2;
         else return a1;
     }
 
-    public void endCombat(Player winner,Monster loser){
+    private void rewardPlayerXP(Player winner,Monster loser){
         winner.increaseXP((loser.getExpReward()));
     }
 
-    public void gameOver(Player player){
+    private void gameOver(Player player){
         player.getLevel().setCurrentLevel(1);
         player.getPlayerInventory().clearInventory();
         player.getPotionInventory().clearPotionInventory();
